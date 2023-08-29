96a65841df7f39cb7ff88d7b3c56bf975d2f7839
Gnezd <ycdzeng@gmail.com>
2023-08-29 13:35:06 +0800

    rid of dropbox attrs

--- tests/ui.rb
+++ tests/ui.rb
@@ -5,6 +5,19 @@ class SpectTuner
     @param_panel = TkFrame.new(tkroot) {grid('row': 0, 'column': 0, 'sticky': 'nsew', rowspan: 2)}
     @td_canvas = TkCanvas.new(tkroot) {grid('row': 0, 'column': 1, 'sticky': 'nsew')}
     @fd_canvas = TkCanvas.new(tkroot) {grid('row': 1, 'column': 1, 'sticky': 'nsew')}
+    @ngserver = IO.popen({}, 'python ./tests/ngserver.py', "r+")
+  end
+  
+  def open_datafile
+    # Py: set fname and load
+    # Get title
+  end
+
+  def process_spect
+    # Python instruction: process with passed parameters
+    # Python instruction: export data
+    # Gnuplot
+    # update canvas
   end
 end
 
